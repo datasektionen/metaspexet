73e9d7f682073f00de1d7bd7f86f9f6e5ba3fc84
adamsjo <adamsjo@kth.se>
2022-04-18 18:07:21 +0200

    Huge update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,11 @@ import { Link } from 'gatsby';
 const Navbar = () => {
     return (
         <nav className={styles.links}>
-            <h1>Hello world from navbar</h1>
+            <h1>METASPEXET</h1>
             <Link to="/">Home</Link>
             <Link to="/about/index-about">About</Link>
-            <Link to="/">test3</Link>
+            <Link to="/">Tickets</Link>
+            <Link to="/">News</Link>
         </nav>
     );
 };
---
+++ src/constant.md
@@ -0,0 +1,15 @@
+
+
+Bakgrundpalett:
+Ljusgrå: rgb(203,203,203)
+Mörkgrå: rgb(92,92,92)
+Grön: rgb(30,99,68)
+Beige: rgb(221,209,168)
+Brun: rgb(92,59,41)
+
+Förgrundspalett (Utstickande):
+Orange: rgb(239,161,18)
+Mörkblå: rgb(51,53,221)
+Ljusblå:rgb(127,163,237)
+Gul: rgb(255,251,20)
+Röd: rgb(237,50,52)
