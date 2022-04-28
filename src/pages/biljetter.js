d3731855cb260d132fe965bbda25e6672e8b7f58
Johan Lam <johanlam@Johans-MacBook-Pro.local>
2022-04-28 15:02:18 +0200

    feat: redirect /biljetter to ticket page

---
+++ src/pages/biljetter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+window.location.replace('https://metaspexet.confetti.events/');
+
+const Biljetter = () => {
+    return <Layout></Layout>;
+};
+
+export default Biljetter;
