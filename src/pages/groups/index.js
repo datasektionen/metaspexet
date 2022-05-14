b28352ee605801ac3718ffaedb84078175f25137
Johan Lam <johanlam@Johans-MacBook-Pro.local>
2022-05-14 03:33:19 +0200

    feat: display group images

--- src/pages/groups/index.js
+++ src/pages/groups/index.js
@@ -1,73 +1,69 @@
 import React from 'react';
 import Layout from '../../components/Layout';
 import * as styles from '../../styles/index-about.module.css';
+import Arr from '../../../assets/images/group/Arr.png';
+import Skadis from '../../../assets/images/group/skådis.png';
+import Chefer from '../../../assets/images/group/Chefsgrupp-Tot.png';
+import Direqtionen from '../../../assets/images/group/Direqtionen.png';
+import Missing from '../../../assets/images/group/missing.png';
+import Koren from '../../../assets/images/group/koren.png';
+import Bygg from '../../../assets/images/group/bygg.png';
+import Dans from '../../../assets/images/group/dans.png';
+import Requisita from '../../../assets/images/group/requisita.png';
+import Fotofilm from '../../../assets/images/group/fotofilm.png';
+import Kostym from '../../../assets/images/group/kostym.png';
+import Lyrique from '../../../assets/images/group/lyrique.png';
+import Lol from '../../../assets/images/group/LOL.png';
+import Orqester from '../../../assets/images/group/Orqester.png';
+import Manus from '../../../assets/images/group/manus.png';
+import Smink from '../../../assets/images/group/Smink_Hår.png';
+import Grafik from '../../../assets/images/group/grafik.png';
+import Webb from '../../../assets/images/group/webb.png';
+import Spexm from '../../../assets/images/group/spexm.png';
 
-const groupImages = new Array();
-for (let index = 0; index < 20; index++) {
-    groupImages[index] = undefined;
-}
-groupImages[0] = '../../../assets/images/group/Arr.png';
-groupImages[1] = '../../../assets/images/group/Chefsgrupp-Tot.png';
-groupImages[2] = '../../../assets/images/group/Direqtionen.png';
-groupImages[3] = '../../../assets/images/group/missing.png';
-groupImages[4] = '../../../assets/images/group/missing.png';
-groupImages[5] = '../../../assets/images/group/koren.png';
-groupImages[6] = '../../../assets/images/group/bygg.png';
-groupImages[7] = '../../../assets/images/group/dans.png';
-groupImages[8] = '../../../assets/images/group/requisita.png';
-groupImages[9] = '../../../assets/images/group/fotofilm.png';
-groupImages[10] = '../../../assets/images/group/kostym.png';
-groupImages[11] = '../../../assets/images/group/lyrique.png';
-groupImages[12] = '../../../assets/images/group/LOL.png';
-groupImages[13] = '../../../assets/images/group/Arr.png';
-groupImages[14] = '../../../assets/images/group/Orqester.png';
-groupImages[15] = '../../../assets/images/group/manus.png';
-groupImages[16] = '../../../assets/images/group/Smink_Hår.png';
-groupImages[17] = '../../../assets/images/group/grafik.png';
-groupImages[18] = '../../../assets/images/group/webb.png';
-groupImages[19] = '../../../assets/images/group/spexm.png';
+const chefer = {
+    groupName: 'Chefsgruppen',
+    pictureName: Chefer,
+};
 
 const groups = [
-    { groupName: 'Squådis', pictureName: groupImages[0] },
-    { groupName: 'Chefsgruppen', pictureName: groupImages[1] },
-    { groupName: 'Dirrarna', pictureName: groupImages[2] },
-    { groupName: 'Ekonomi', pictureName: groupImages[3] },
-    { groupName: 'Synk', pictureName: groupImages[4] },
-    { groupName: 'Kören', pictureName: groupImages[5] },
-    { groupName: 'Bygg', pictureName: groupImages[6] },
-    { groupName: 'Dans', pictureName: groupImages[7] },
-    { groupName: 'Requisita', pictureName: groupImages[8] },
-    { groupName: 'Fotofilm', pictureName: groupImages[9] },
-    { groupName: 'Kostym', pictureName: groupImages[10] },
-    { groupName: 'Lyrique', pictureName: groupImages[11] },
-    { groupName: 'Ljud & ljus', pictureName: groupImages[12] },
-    { groupName: 'Arr', pictureName: groupImages[13] },
-    { groupName: 'Orquestern', pictureName: groupImages[14] },
-    { groupName: 'Manus', pictureName: groupImages[15] },
-    { groupName: 'Sminq', pictureName: groupImages[16] },
-    { groupName: 'Grafik', pictureName: groupImages[17] },
-    { groupName: 'Webb', pictureName: groupImages[18] },
-    { groupName: 'Spexm', pictureName: groupImages[19] },
+    { groupName: 'Arr', pictureName: Arr },
+    { groupName: 'Squådis', pictureName: Skadis },
+
+    { groupName: 'Dirrarna', pictureName: Direqtionen },
+    { groupName: 'Ekonomi', pictureName: Missing },
+    { groupName: 'Synk', pictureName: Missing },
+    { groupName: 'Kören', pictureName: Koren },
+    { groupName: 'Bygg', pictureName: Bygg },
+    { groupName: 'Dans', pictureName: Dans },
+    { groupName: 'Requisita', pictureName: Requisita },
+    { groupName: 'Fotofilm', pictureName: Fotofilm },
+    { groupName: 'Kostym', pictureName: Kostym },
+    { groupName: 'Lyrique', pictureName: Lyrique },
+    { groupName: 'Ljud & ljus', pictureName: Lol },
+    { groupName: 'Orquestern', pictureName: Orqester },
+    { groupName: 'Manus', pictureName: Manus },
+    { groupName: 'Sminq', pictureName: Smink },
+    { groupName: 'Grafik', pictureName: Grafik },
+    { groupName: 'Webb', pictureName: Webb },
+    { groupName: 'Spexm', pictureName: Spexm },
 ];
 
-const testArr = [
-    { a: 'test1', b: 'path1' },
-    { a: 'test2', b: 'path2' },
-];
-
-console.log(testArr);
 const Groups = () => {
     return (
         <Layout>
+            <div>
+                <h1>{chefer.groupName}</h1>
+                <img src={chefer.pictureName} alt="" />
+            </div>
+
             <div className={styles.indexabout}>
-                <h1>Hello</h1>
-                {groups.map((groupsName, pictureName) => (
+                {groups.map((group) => (
                     <div>
-                        <img src={pictureName.pictureName} />
-                        {groupsName.groupName}
+                        <h1>{group.groupName}</h1>
+                        <img src={group.pictureName} alt={group.groupName} />
                     </div>
                 ))}
-                {/* {groups.map((groupsName, pictureName) => <div><img src={pictureName.pictureName}/>{groupsName.groupName}</div>)} */}
             </div>
         </Layout>
     );
